139f7ac6ae8e0c6df5d907ff7076901ef3de0fd2
FabianGal45 <fabiangal.45@gmail.com>
2022-04-01 14:42:19 +0100

    test

--- app/src/main/java/com/example/mythirdattempt/MainActivity.java
+++ app/src/main/java/com/example/mythirdattempt/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mythirdattempt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,8 +8,11 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,10 +27,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDatabase = FirebaseDatabase.getInstance("https://testforc-7b1f7-default-rtdb.europe-west1.firebasedatabase.app/").getReference("message");;
+        mDatabase = FirebaseDatabase.getInstance("https://testforc-7b1f7-default-rtdb.europe-west1.firebasedatabase.app/").getReference("Name");;
 //        mDatabase.setValue("Hello, World!");
 //        mUserList = (ListView) findViewById(R.id.user_List);
         mNameView = (TextView) findViewById(R.id.nameTV);
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String name = snapshot.getValue().toString();
+                mNameView.setText("Name: "+ name);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
 
